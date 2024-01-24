398fc81d3f8c5b85328e29a1ade5cdc03f7d6621
Lawrence <mwirigimburugu81@gmail.com>
2024-01-24 15:13:51 +0300

    complete

--- server/app.py
+++ server/app.py
@@ -15,4 +15,3 @@ db.init_app(app)
 
 if __name__ == '__main__':
     app.run(port=5555)
-
--- server/models.py
+++ server/models.py
@@ -26,3 +26,4 @@ class Pet(db.Model):
 
     def __repr__(self):
         return f'<Pet {self.name}, {self.species}>'
+
